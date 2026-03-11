ee445fe003f7d3f0dc15eea87691430aa70b97ac
Natã Borges <borgesnatan412@gmail.com>
2026-03-11 21:45:58 +0000

    feat: classe Refeicao finalizada com calculo de calorias

--- src/Refeicao.java
+++ src/Refeicao.java
@@ -2,6 +2,27 @@ public class Refeicao {
     private double quantProt;
     private double quantCarb;
     private double quantGordura;
-    private double quantCaloriasTotais;
     private String nome;
+
+    public Refeicao (String nome, double quantProt, double quantCarb, double quantGordura) {
+        this.nome = nome;
+        this.quantProt = quantProt;
+        this.quantCarb = quantCarb;
+        this.quantGordura = quantGordura;
+    }
+    public double getProt() {
+        return this.quantProt;
+    }
+    public double getCarb() {
+        return this.quantCarb;
+    }
+    public double getGordura() {
+        return this.quantGordura;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public double getCaloriasTotais() {
+        return (this.quantProt * 4) + (this.quantCarb * 4) + (this.quantGordura * 9);
+    }
 }
