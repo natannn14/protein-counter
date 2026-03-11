ec2d1bc9f69e95d7ae4d2651df4667fd0c9e6251
natannn14 <borgesnatan412@gmail.com>
2026-03-10 23:47:25 -0300

    Refatoração: Implementação da classe Usuario com Encapsulamento e lógica de meta

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,11 +1,45 @@
 public class Usuario {
+    private String nome;
     private double peso;
+    private double altura;
     private double metaProteina;
-    private double proteinaConsumida;
 
-    public Usuario(double peso, double fatorProteina) {
+    public Usuario (String nome, double peso, double altura) {
+        this.nome = nome;
         this.peso = peso;
-        metaProteina = peso * fatorProteina;
-        proteinaConsumida = 0;
+        this.altura = altura;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public double getPeso(){
+        return this.peso;
+    }
+    public double getAltura(){
+        return this.altura;
+    }
+    public double getMetaProteina(){
+        return this.metaProteina;
+    }
+    public void setPeso(double novoPeso){
+        if (novoPeso > 0) {
+            this.peso = novoPeso;
+        } else {
+            System.out.println("Erro: seu peso deve ser maior que zero");
+        }
+
+    }
+    public void setAltura (double novaAltura) {
+        this.altura = novaAltura;
+    }
+    public void setMetaProteina (double novaMetaProteina) {
+        if (novaMetaProteina > 2.4 * peso) {
+            System.out.println("Essa meta pode ser prejudicial a saúde");
+        } else {
+            this.metaProteina = novaMetaProteina;
+        }
+    }
+    public double calcularSugestaoMeta (double fator) {
+        return this.peso * fator;
     }
 }
